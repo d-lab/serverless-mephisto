7ba7e26c81f6e755db701c6d991cccfd731aa3d8
Cong Thanh Vu <congthanh3181@gmail.com>
2023-11-12 16:27:00 +0700

    test caching

--- stacks/DockerImageBuilder.ts
+++ stacks/DockerImageBuilder.ts
@@ -52,7 +52,7 @@ export default class DockerImageBuilder {
         }
 
         // console.log("Env: CDK_DOCKER: " + process.env.CDK_DOCKER);
-        process.env.CDK_DOCKER = "docker-buildx";
+        process.env.CDK_DOCKER = '#!/bin/bash\nif [ "$3" == "build" ]\nthen set -- "${@:4} --load"\nfi\n~/.docker/cli-plugins/docker-buildx build $@'
 
         const image = new DockerImageAsset(this.stack, 'CDKDockerImage', {
             directory: this.path,

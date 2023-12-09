5c6be972c084ce5465472a8c3c45550acdbba871
Jay Huynh <jay.huynh@nib.com.au>
2023-12-09 19:34:19 +1000

    Add log filter

--- actions/utils/commandExecution.ts
+++ actions/utils/commandExecution.ts
@@ -2,14 +2,14 @@ import * as subProcess from 'child_process';
 import { info } from '@actions/core';
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-export const execAsync = async (command: string, options: any = undefined) => {
+export const execAsync = async (command: string, options: any = undefined, filter: any = undefined) => {
     let closed = false;
     const stream = subProcess.exec(command, options);
     stream.stdout?.on('data', (data) => {
-        info(data);
+        info(applyFilter(data, filter));
     });
     stream.stderr?.on('data', (data) => {
-        info(data);
+        info(applyFilter(data, filter));
     });
     stream.stdout?.on('close', () => {
         closed = true;
@@ -17,4 +17,15 @@ export const execAsync = async (command: string, options: any = undefined) => {
     while (!closed) {
         await sleep(5000);
     }
+}
+
+function applyFilter(data: any, filter: any) {
+    if (!filter) {
+        return data;
+    }
+    let result = data;
+    if (filter.replace) {
+        result = result.replaceAll(filter.replace.pattern, filter.replace.by);
+    }
+    return result;
 }

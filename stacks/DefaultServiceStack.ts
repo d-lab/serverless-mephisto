25c469d693f3b757c3886cb179e8abfcd347de53
Cong Thanh Vu <congthanh3181@gmail.com>
2023-12-17 21:41:11 +0700

    update task name

--- stacks/DefaultServiceStack.ts
+++ stacks/DefaultServiceStack.ts
@@ -156,7 +156,7 @@ export function DefaultServiceStack({ stack }: StackContext) {
         }
     });
 
-    const lambdaEfsMountedFolder = "/efs";
+    const lambdaEfsMountedFolder = "/mnt/efs/rsmp";
 
     const syncS3Lambda = new lambdaNode.NodejsFunction(stack, `${stack.stackName}-sync-s3`, {
         runtime: lambda.Runtime.NODEJS_18_X,

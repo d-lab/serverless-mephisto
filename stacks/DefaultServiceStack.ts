0cd69a207a560a145bc2e6ac36b4210a134b8503
Jay Huynh <jay.huynh@nib.com.au>
2024-01-08 21:29:45 +1000

    Pass env when docker running

--- stacks/DefaultServiceStack.ts
+++ stacks/DefaultServiceStack.ts
@@ -79,7 +79,18 @@ export function DefaultServiceStack({ stack }: StackContext) {
         stopTimeout: Duration.seconds(Number(process.env.STOP_TIMEOUT || 30)),
         environment: {
             APP_ENV: process.env.APP_ENV as string,
-            APP_NAME: process.env.APP_NAME as string
+            APP_NAME: process.env.APP_NAME as string,
+            GIT_USER_EMAIL: process.env.GIT_USER_EMAIL as string,
+            GIT_USER_NAME: process.env.GIT_USER_NAME as string,
+            MTURK_NAME: process.env.MTURK_NAME as string,
+            MTURK_TYPE: process.env.MTURK_TYPE as string,
+            MTURK_ACCESS_KEY_ID: process.env.MTURK_ACCESS_KEY_ID as string,
+            MTURK_SECRET_ACCESS_KEY: process.env.MTURK_SECRET_ACCESS_KEY as string,
+            DOTNETRC: process.env.DOTNETRC as string,
+            HEROKU_API_KEY: process.env.HEROKU_API_KEY as string,
+            PROLIFIC_API_KEY: process.env.PROLIFIC_API_KEY as string,
+            AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID as string,
+            AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY as string,
         },
         portMappings: [{ containerPort: Number(process.env.CONT_PORT || 3000) }]
     });
